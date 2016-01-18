30d5afca4a03ff423940a19548526f954c571bad
Rocky Madden <git@rockymadden.com>
2016-01-18 10:11:16 -0700

    Tests broken out into unit and integration

---
+++ test/integration/suite.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+function setup() {
+  if [ -z "${HYKES_PROVISIONER_BLUEPRINT}" ] || \
+     [ -z "${HYKES_PROVISIONER_PASSWORD}" ] || \
+     [ -z "${HYKES_PROVISIONER_TOKEN}" ]; then
+
+    skip "Environment variables should be defined"
+  fi
+}
