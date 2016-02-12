da00bb115e6b3a63414a52adff58e4331cf16f6a
Rocky Madden <git@rockymadden.com>
2016-02-12 11:15:19 -0700

    Pulled environment check into its own function

--- test/integration/suite.bash
+++ test/integration/suite.bash
@@ -1,9 +1,10 @@
 #!/usr/bin/env bats
 
-function setup() {
-  if [ -z "${HYKES_PROVISIONER_PATH}" ]; then
-    skip 'Environment variables should be defined'
-  fi
+function only-with-env() {
+  if [ -z "${HYKES_PROVISIONER_PATH}" ]; then skip 'Environment variables should be defined'; fi
+}
 
+function setup() {
+  only-with-env
   build/bin/hykes-provisioner init "${HYKES_PROVISIONER_PATH}"
 }
