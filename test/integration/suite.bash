b99b2748e63155f78db0e331af0e905888acf646
Rocky Madden <git@rockymadden.com>
2016-03-17 08:57:10 -0600

    Refactored and simplified tests to use hykes-spec

--- test/integration/suite.bash
+++ test/integration/suite.bash
@@ -1,20 +1,13 @@
 #!/usr/bin/env bats
 
 function clone-repo() {
-  git clone "${HYKES_PROVISION_URL}" "${HYKES_PROVISION_PATH}"
+  git clone git@github.com:cloud-elements/hykes-spec.git build/tmp/hykes-spec
 }
 
 function exists-repo() {
-  test -d "${HYKES_PROVISION_PATH}"
+  test -d build/tmp/hykes-spec
 }
 
-function should-have-env() {
-  if [ -z "${HYKES_PROVISION_PATH}" ] || [ -z "${HYKES_PROVISION_URL}" ]; then
-    skip 'Environment variables should be available'
-  fi
-}
-
-# TODO: Remove assumption that blueprint is not encrypted
 function setup() {
-  should-have-env && if ! exists-repo; then clone-repo; fi && cd "${HYKES_PROVISION_PATH}"
+  if ! exists-repo; then clone-repo; fi && cd build/tmp/hykes-spec
 }
