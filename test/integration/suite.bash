bddb105042a9efd114766a11e1950350209882b8
Rocky Madden <git@rockymadden.com>
2016-03-18 10:44:49 -0600

    Fixed edge case dependency issue with blueprint and spec
    An issue arises, due to the continous release approach of both
    blueprint and spec, in which potentially breaking changes could
    be picked up from master. Instead, we want to use the latest tag
    for each of the dependent projects instead.

--- test/integration/suite.bash
+++ test/integration/suite.bash
@@ -2,6 +2,7 @@
 
 function clone-repo() {
   git clone git@github.com:cloud-elements/hykes-spec.git build/tmp/hykes-spec
+  (cd build/tmp/hykes-spec && git checkout $(git describe --tags $(git rev-list --tags --max-count=1)))
 }
 
 function exists-repo() {
